ab2271d6c8fa701f94d475f13a312abb2fbe3b33
Majeedat Abdulwahab <majeedatabdulwahabayo@gnail.com>
2024-07-08 11:28:58 +0100

    changes here and there.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@supabase/supabase-js']
+     external: /^@supabase\/supabase-js/
     }
   }
 })
