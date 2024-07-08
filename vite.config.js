a083a1f994ffed6eb0e290aaec411587798a60bc
Majeedat Abdulwahab <majeedatabdulwahabayo@gnail.com>
2024-07-08 11:52:36 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  build: {
+    rollupOptions: {
+     external: /^@supabase\/supabase-js/
+    }
   }
 })
